0f5a569ceff40f17db3be8e1a68bd09a0cd33cf5
Subham Subhasis Patra <43811917+SubhamSubhasisPatra@users.noreply.github.com>
2024-05-18 13:54:34 +0530

    added some copied style like so it look like premium :-) , still the same old crap

--- src/components/WindowControls/WindowControls.jsx
+++ src/components/WindowControls/WindowControls.jsx
@@ -1,22 +1,29 @@
 // WindowControls.js
-import React from "react";
+import React, { useEffect } from "react";
 import { appWindow } from "@tauri-apps/api/window";
+import closeIcon from "../../assets/windows/close.png";
+import minimizeIcon from "../../assets/windows/minimize.png";
+import maximizeIcon from "../../assets/windows/maximize.png";
 import "./WindowControls.css";
 
 export default function WindowControls() {
+  const handleMinimize = () => appWindow.minimize();
+  const handleMaximize = () => appWindow.toggleMaximize();
+  const handleClose = () => appWindow.close();
 
-    const handleMinimize = () => appWindow.minimize();
-    const handleMaximize = () => appWindow.toggleMaximize();
-    const handleClose = () => appWindow.close();
-  
-    return (
-      <div className="titlebar">
-        <div className="window-controls">
-        <div className="draggable-area"></div>
-          <button className="window-button close" onClick={handleClose} />
-          <button className="window-button minimize" onClick={handleMinimize} />
-          <button className="window-button maximize" onClick={handleMaximize} />
+  return (
+    <div className="titlebar" data-tauri-drag-region="true">
+      <div className="window-controls">
+        <div className="window-button" onClick={handleClose}>
+          <img src={closeIcon} alt="Close" className="window-icon" />
+        </div>
+        <div className="window-button" onClick={handleMinimize}>
+          <img src={minimizeIcon} alt="Minimize" className="window-icon" />
+        </div>
+        <div className="window-button" onClick={handleMaximize}>
+          <img src={maximizeIcon} alt="Maximize" className="window-icon" />
         </div>
       </div>
-    );
+    </div>
+  );
 }
