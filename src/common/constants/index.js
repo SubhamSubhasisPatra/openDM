f3a017ae6f064881a22a755b2e91cb0a3908dc25
Subham Subhasis Patra <43811917+SubhamSubhasisPatra@users.noreply.github.com>
2024-05-23 23:52:31 +0530

    Added Constants for all the filter

--- src/common/constants/index.js
+++ src/common/constants/index.js
@@ -5,3 +5,7 @@ export const WINDOWS = "Windows";
 export const LINUX = "Linux";
 
 export const FILTER_ALL = 'All';
+export const FILTER_RUNNING = 'Running';
+export const FILTER_SUSPENDED = 'Suspended';
+export const FILTER_COMPLETE = 'Complete';
+export const FILTER_INCOMPLETE = 'Incomplete';
