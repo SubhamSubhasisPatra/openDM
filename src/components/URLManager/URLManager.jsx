2d66fbc1c68b3ac0be992b664732b2084d0fb9a8
Subham Subhasis Patra <43811917+SubhamSubhasisPatra@users.noreply.github.com>
2024-05-25 12:15:36 +0530

    added Min len warning

--- src/components/URLManager/URLManager.jsx
+++ src/components/URLManager/URLManager.jsx
@@ -5,11 +5,10 @@ import {invoke} from "@tauri-apps/api/tauri";
 import {save} from "@tauri-apps/api/dialog";
 
 export default function URLManager({onDWLDListChange, filteredCount, filteredType}) {
-
     const [fileId, setFileId] = useState(0);
     const [downloadList, setDownloadList] = useState([]);
     const [URL, setURL] = useState('');
-
+    const [warning, setWarning] = useState(false);
 
     useEffect(() => {
         invoke('get_all_file_info').then(results => setDownloadList(results));
@@ -17,6 +16,11 @@ export default function URLManager({onDWLDListChange, filteredCount, filteredTyp
     }, [downloadList]);
 
     const downloadClickHandler = async () => {
+        if (URL.trim().length === 0) {
+            setWarning(true);
+            return;
+        }
+
         console.log("Input Value:", URL);
         setURL('');
 
@@ -50,6 +54,9 @@ export default function URLManager({onDWLDListChange, filteredCount, filteredTyp
 
     const handleInputChange = (event) => {
         setURL(event.target.value);
+        if (event.target.value.trim().length > 0) {
+            setWarning('');
+        }
     };
 
     const handleKeyPress = async (event) => {
@@ -62,14 +69,15 @@ export default function URLManager({onDWLDListChange, filteredCount, filteredTyp
         <div className="flex items-center justify-between mb-4">
             <div className="flex items-center space-x-4">
                 <h2 className="text-xl font-semibold text-zinc-800 dark:text-zinc-200">{filteredType}</h2>
-                <span
-                    className="text-l bg-zinc-100 dark:bg-zinc-700 p-2 rounded-lg text-zinc-600 dark:text-zinc-400">{filteredCount}</span>
+                <span className="text-l bg-zinc-100 dark:bg-zinc-700 p-2 rounded-lg text-zinc-600 dark:text-zinc-400">
+                    {filteredCount}
+                </span>
             </div>
             <div className="flex items-center space-x-2">
                 <input
                     type="text"
                     placeholder="Download URL"
-                    className="px-4 py-2 border rounded-lg text-zinc-800 dark:text-zinc-200 bg-zinc-100 dark:bg-zinc-700 border-zinc-200 dark:border-zinc-600 border-transparent hover:border-zinc-300"
+                    className={`px-4 py-2 border rounded-lg text-zinc-800 dark:text-zinc-200 bg-zinc-100 dark:bg-zinc-700 border-zinc-200 dark:border-zinc-600 border-transparent hover:border-zinc-300`}
                     value={URL}
                     onChange={handleInputChange}
                     onKeyPress={handleKeyPress}
