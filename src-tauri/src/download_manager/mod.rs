a8c3f11ed0e77e076153c0d83a815e9aa6cc90b3
Subham Subhasis Patra <43811917+SubhamSubhasisPatra@users.noreply.github.com>
2024-06-01 14:50:35 +0530

    Improved multipart download

--- src-tauri/src/download_manager/mod.rs
+++ src-tauri/src/download_manager/mod.rs
@@ -10,6 +10,8 @@ use reqwest::Client;
 use tokio::sync::Mutex;
 use tokio::task;
 use tokio::time::{sleep, Duration, Instant};
+use futures_util::StreamExt;
+
 
 use crate::commands::get_default_download_path;
 use serde::{Deserialize, Serialize};
@@ -28,6 +30,7 @@ pub struct FileInfo {
 struct ChunkProgress {
     index: usize,
     progress: usize,
+    total_bytes: u64
 }
 
 async fn get_file_size(client: &Client, url: &str) -> Result<u64> {
@@ -45,55 +48,81 @@ async fn supports_partial_content(client: &Client, url: &str) -> Result<bool> {
     Ok(res.headers().get(reqwest::header::ACCEPT_RANGES).is_some())
 }
 
-async fn download_chunk(
-    client: &Client,
-    start: u64,
-    end: u64,
-    index: usize,
-    url: &str,
-    progress: Arc<Mutex<Vec<ChunkProgress>>>,
-) -> Result<(usize, Vec<u8>)> {
-    let res = client
-        .get(url)
+
+async fn download_chunk(client: &Client, start: u64, end: u64, index: usize, url: &str, progress: Arc<Mutex<Vec<ChunkProgress>>>) -> Result<(usize, Vec<u8>)> {
+    let res = client.get(url)
         .header(reqwest::header::RANGE, format!("bytes={}-{}", start, end))
-        .send()
-        .await?;
+        .send().await?;
+
     let status = res.status();
     if status.is_success() {
-        let chunk = res.bytes().await?;
-        // Simulate downloading progress (update every second)
-        for _ in 0..10 {
-            sleep(Duration::from_millis(10)).await; // Wait 100ms (simulate 1/10th of a second)
+        let mut stream = res.bytes_stream();
+        let mut chunk = Vec::new();
+        let total_bytes = end - start + 1;
+        let mut downloaded = 0;
+
+        while let Some(item) = stream.next().await {
+            let bytes = item?;
+            chunk.extend_from_slice(&bytes);
+            downloaded += bytes.len() as u64;
+
+            // Update progress
             let mut progress = progress.lock().await;
-            progress[index].progress += 10; // Increase progress by 10%
-            println!("Chunk {} Progress: {}%", index, progress[index].progress);
+            progress[index].progress = ((downloaded as f64 / total_bytes as f64) * 100.0) as usize;
+            println!("Chunk {} Progress: {}%, Downloaded: {} bytes",
+                     index,
+                     progress[index].progress,
+                     downloaded);
+            drop(progress); // Release the lock
         }
-        Ok((index, chunk.to_vec()))
+
+        Ok((index, chunk))
     } else {
         Err(anyhow!("Failed to download chunk with status: {}", status))
     }
 }
 
-async fn download_multipart(client: &Client, url: &str, file_payload: &FileInfo) -> Result<()> {
+fn calculate_chunk_size(file_size: u64) -> (u64, usize) {
+    let num_chunks: usize;
+    let min_chunk_size: u64;
+
+    if file_size < 100 * 1024 * 1024 { // less than 100MB
+        num_chunks = (file_size / (20 * 1024 * 1024)).max(1) as usize;
+        min_chunk_size = 20 * 1024 * 1024; // 20MB
+    } else if file_size < 1024 * 1024 * 1024 { // less than 1GB
+        num_chunks = 12;
+        min_chunk_size = (file_size / 12) as u64;
+    } else { // more than 1GB
+        num_chunks = 24;
+        min_chunk_size = (file_size / 24) as u64;
+    }
+
+    let chunk_size = (file_size / num_chunks as u64).max(min_chunk_size);
+
+    (chunk_size, num_chunks)
+}
+
+
+async fn download_multipart(client: &Client, url: &str, file_payload: FileInfo) -> Result<()> {
     let file_size = get_file_size(client, url).await?;
 
     // Define minimum and maximum chunk sizes
-    let min_chunk_size = 10 * 1024 * 1024; // 10 MB
-    let max_chunk_size = 100 * 1024 * 1024; // 100 MB
-
-
-    // Calculate the number of chunks
-    let num_chunks = std::cmp::max(1, std::cmp::min(100, (file_size / min_chunk_size) as usize));
-    let chunk_size = std::cmp::max(min_chunk_size, file_size / num_chunks as u64);
+    let (chunk_size, num_chunks) = calculate_chunk_size(file_size);
 
     // Initialize progress for each chunk
-    let progress = Arc::new(Mutex::new(vec![
-        ChunkProgress {
-            index: 0,
-            progress: 0
+    let progress = Arc::new(Mutex::new(vec![ChunkProgress { index: 0, progress: 0, total_bytes: chunk_size }; num_chunks]));
+
+    println!("Total chunks: {}", num_chunks);
+    for i in 0..num_chunks {
+        let start = i as u64 * chunk_size;
+        let end = if i == num_chunks - 1 {
+            file_size - 1
+        } else {
+            (i + 1) as u64 * chunk_size - 1
         };
-        num_chunks
-    ]));
+        let chunk_size = end - start + 1;
+        println!("Chunk {}: {} - {} ({} bytes)", i, start, end, chunk_size);
+    }
 
     let mut handles = vec![];
 
@@ -125,18 +154,13 @@ async fn download_multipart(client: &Client, url: &str, file_payload: &FileInfo)
 
     let file_name = file_payload.file_name.clone(); // Clone the file name to move into the closure
 
-    // load the app config default download path and create the file
-    let default_download_path = get_default_download_path().unwrap();
-    let download_path = Path::new(&default_download_path).join(file_name);
-
     task::spawn_blocking(move || {
-        let mut file = File::create(&download_path)?;
+        let mut file = File::create(&file_name)?;
         for (_, chunk) in chunks {
             file.write_all(&chunk)?;
         }
         Result::<(), std::io::Error>::Ok(())
-    })
-    .await??;
+    }).await??;
 
     Ok(())
 }
@@ -149,11 +173,11 @@ pub async fn download_start(url: &str, file_payload: FileInfo) -> Result<(), Str
         .await
         .map_err(|e| e.to_string())?;
 
-    println!("Type : {}", can_download_multipart);
+    println!("Support Multipart : {}", can_download_multipart);
 
     if can_download_multipart {
         let start = Instant::now();
-        download_multipart(&client, &url, &file_payload)
+        download_multipart(&client, &url, file_payload.clone())
             .await
             .map_err(|e| e.to_string())?;
         let duration = start.elapsed();
